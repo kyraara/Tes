e3312c7ae3546248a51f75870ced52b6e010a0fd
KyRaFitzz <refkysyahputra13@gmail.com>
2025-08-20 08:29:50 +0700

    init: setup React/Next + Tailwind v4

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <main className="min-h-screen grid place-items-center">
+      <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind v4 🚀</h1>
+    </main>
+    </>
+  )
+}
+
+export default App
